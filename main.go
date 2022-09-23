6d59f607042ac27df4d01cf5f773a57a4dd3e4a5
chdavidjohn <65406323+chdavidjohn@users.noreply.github.com>
2022-09-23 14:09:32 +0530

    v1

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World - Run 2"}
+		res := &response{Message: "Kubernetes service v1"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
